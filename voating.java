bb30ff9790f4956e713c83104b3dce420dfabd18
Naveen Kushwaha <naveenkushwaha7804@gmail.com>
2025-11-08 10:54:05 +0530

    programme for check a person can give vote or not

---
+++ voating.java
@@ -0,0 +1,14 @@
+package Other;
+import java.util.Scanner;
+class voating {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int a = sc.nextInt();
+        if ( a > 18)
+            System.out.println("you are able to voting");
+        else
+            System.out.println(" you are not able to voting ");
+    }
+}
+
